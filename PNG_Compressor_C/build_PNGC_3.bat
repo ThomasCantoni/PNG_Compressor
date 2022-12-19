e61aad3fc2978c00259d4b9e2c5ab7cba62b6792
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-12-19 22:38:16 +0100

    more progress

--- PNG_Compressor_C/build_PNGC_3.bat
+++ PNG_Compressor_C/build_PNGC_3.bat
@@ -1,5 +1,5 @@
 @echo off 
 
-clang -o .\bin\PNG_Compressor3.exe -I src\SDL2-2.26.0\include -I include\ -L src/SDL2-2.26.0/lib/x64   .\src\PNG_Compressor3.c -lSDL2
+clang -o .\bin\PNG_Compressor3.exe -I src\SDL2-2.26.0\include -I include\ -I include\zlib -L src/SDL2-2.26.0/lib/x64 -L include\zlib  .\src\PNG_Compressor3.c -lSDL2 -lzlibwapi
 
 .\bin\PNG_Compressor3.exe .\materials\test2.png
--- PNG_Compressor_C/src/PNG_Compressor3.c
+++ PNG_Compressor_C/src/PNG_Compressor3.c
@@ -1,9 +1,11 @@
-#include "lodepng.c"
+//#include "lodepng.c"
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <stdio.h>
 #include <math.h>
 #include "LinkedListElements.h"
+#include "zlib.h"
+
 
 #define PNG_SIGNATURE "\x89PNG\r\n\x1a\n"
 typedef struct chunk
@@ -22,7 +24,7 @@ typedef struct png_image
 {
   char* filename;
   size_t size;
-  char* raw_data;
+  //char* raw_data;
   unsigned int width;
   unsigned int height;
   chunk_node* IHDR;
@@ -35,16 +37,133 @@ size_t get_file_length( FILE* file_to_open )
     rewind(file_to_open);
     return len;
 }
-void display_raw_image_data(png_image* image)
+void display_raw_image_data(char* image,size_t image_size)
 {
-    for (size_t i = 0; i < image->size;++i)
+    for (size_t i = 0; i < image_size;++i)
     {
-        printf("0x%i \t",image->raw_data[i]);
+        printf("0x%i \t",image[i]);
        if((i +1)% 16 == 0)
        {
             printf("|\n");
        }
     }
+}
+static unsigned int data_to_int(unsigned char *data, size_t data_length)
+{
+    if (data_length > 4) return 0;
+
+    unsigned int value = 0;
+    unsigned int multiplier = 1;
+    for (int i = data_length - 1; i >= 0; i--)
+    {
+        unsigned char nibble = data[i];
+        value += nibble * multiplier;
+        multiplier *= 256;
+    }
+    return value;
+}
+char* get_png_raw_data(FILE* f,char* buffer,size_t* length)
+{
+    length = get_file_length(f);
+    buffer = calloc( 1, length +1);
+
+    if( !buffer ) fclose(f),fputs("memory alloc fails",stderr),exit(1);
+    
+    size_t i = fread(buffer , length, 1 , f);
+    
+    /* copy the file into the buffer */
+    if(!buffer)
+    {
+      fclose(f);
+      free(buffer);
+      fputs("entire read fails",stderr);
+      return NULL;
+    }
+    return buffer;
+}
+void print_stream(char* string,FILE* file,size_t len)
+{
+    printf("\n");
+    for (size_t i = 0; i < 4; i++)
+    {
+        printf("0x%i at index %i \n",(int)string[i],ftell(file));
+       
+    }
+    printf("stream ended \n");
+}
+struct chunk* read_chunk(FILE **file)
+{
+    
+    printf("\n");
+    //Allocate chunk struct memory
+    chunk *ret = (chunk*)malloc(sizeof(chunk));
+    if (!ret) return NULL;
+
+    //Get data_length
+    unsigned char *four_byte_data = (unsigned char *)malloc(4);
+    if (!four_byte_data)
+    {
+        free(ret);
+        return NULL;
+    }
+    fread(four_byte_data, 4, 1, *file);
+    print_stream(four_byte_data,*file,4);
+    
+    ret->data_length = data_to_int(four_byte_data, 4);
+    //printf("\n%c is %u\n",four_byte_data, ret->data_length);
+    //Get chunk_type
+    ret->type = (unsigned char *)malloc(4);
+    if (!ret->type)
+    {
+        free(ret);
+        free(four_byte_data);
+        return NULL;
+    }
+
+    fread(four_byte_data, 4, 1, *file);
+    printf("\n CHUNK TYPE");
+    print_stream(four_byte_data,*file,4);
+    memcpy(ret->type, four_byte_data, 4);
+
+    //Get chunk_data
+    unsigned char *chunk_data_from_file = (unsigned char *)malloc(ret->data_length);
+    if (!chunk_data_from_file)
+    {
+        free(ret);
+        free(four_byte_data);
+        return NULL;
+    }
+    ret->data = (unsigned char *)malloc(ret->data_length);
+    if (!ret->data)
+    {
+        free(ret);
+        free(four_byte_data);
+        free(chunk_data_from_file);
+        return NULL;
+    }
+    fread(chunk_data_from_file, ret->data_length, 1, *file);
+    printf("%u \n",ret->data_length);
+    //print_stream(chunk_data_from_file,*file,4);
+    memcpy(ret->data, chunk_data_from_file, ret->data_length);
+
+    //Get and check CRC
+    fread(four_byte_data, 4, 1, *file);
+
+    unsigned int expected_crc = data_to_int(four_byte_data, 4);
+    unsigned int exported_crc = crc32(crc32(0, ret->type, 4), ret->data, ret->data_length);
+    if (expected_crc != exported_crc)
+    {
+        printf("Chunk checksum failed {%u} != {%u}\n", expected_crc, exported_crc);
+        free(ret);
+        free(four_byte_data);
+        free(chunk_data_from_file);
+        return NULL;
+    }
+    ret->crc = expected_crc;
+
+    free(four_byte_data);
+    free(chunk_data_from_file);
+    return ret;
 }
 png_image* decode_image(const char* filename)
 {
@@ -57,30 +176,37 @@ png_image* decode_image(const char* filename)
         exit(-1);
     }
     
-    new_image->size = get_file_length(file_to_open);
 
+    size_t length ;
+    char* raw_image_data =0;
+    get_png_raw_data(file_to_open,raw_image_data,&length);
+    new_image->size = length;
     printf("size of image is : %llu \n",new_image->size);
-    char* raw_image_data =  calloc( 1, new_image->size +1);
-
-    if( !raw_image_data ) fclose(file_to_open),fputs("memory alloc fails",stderr),exit(1);
-    
-    size_t i = fread(raw_image_data , new_image->size, 1 , file_to_open);
-    
-    /* copy the file into the buffer */
-    if(!raw_image_data)
-    {
-      fclose(file_to_open);
-      free(raw_image_data);
-      fputs("entire read fails",stderr);
-      exit(1);
-    }
-    new_image->raw_data = raw_image_data;
-    display_raw_image_data(new_image);
-    
-    
+    //new_image->raw_data = raw_image_data;
+    display_raw_image_data(raw_image_data,new_image->size);
+    rewind(file_to_open);
 
     
+    // while (1)
+    // {
+    //     chunk* chunk_in_question = read_chunk(file_to_open);
+    //     if (!chunk_in_question)
+    //     {
+    //         break;
+    //     }
+    //     list_append((struct list_node **)&chunk_list, (struct list_node *)chunk_item_new(chunk));
+    //     unsigned char iend_signature[] = {0x49, 0x45, 0x4e, 0x44};
+    //     if (memcmp(iend_signature, chunk->chunk_type, 4) == 0)
+    //     {
+    //         break;
+    //     }
+    // }
+
+    //new_image->IHDR->chunk =(chunk*) read_chunk(&file_to_open);
+    printf("%s",new_image->IHDR->chunk->data);
     puts("AO");
+
+    
         chunk_node* ihdr = (chunk_node*) malloc(sizeof(ihdr));
         //ihdr->chunk=  csearch_chunk(file_to_open,PNG_SIGNATURE);
         //new_image->IHDR->node->next
@@ -92,22 +218,7 @@ chunk* search_chunk(char* raw_image_data,char* string_to_find)
     return NULL;
 }
 
-chunk* read_chunk(png_image* image)
-{
-  // chunk* chunk;
-  // fread(chunk->data_length, sizeof(unsigned int), 1, f);
-  // fread(chunk->type, sizeof(char), 4, f);
-  // //chunk.data = malloc(chunk.length);
-  // fread(chunk->data, sizeof(unsigned char), chunk->data_length, f);
-  // fread(&chunk.crc, sizeof(unsigned int), 1, f);
-  // //unsigned long actual_crc = crc32(crc32(0, ret->chunk_type, 4), Bytef ,chunk->data_length);
-  // if (chunk.crc != actual_crc)
-  // {
-  //     fprintf(stderr, "Error: chunk checksum failed\n");
-  //     exit(1);
-  // }
-  // return chunk;
-}
+
 
 int main(int argc, char *argv[])
 {
